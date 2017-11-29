fdff61216eb25b465b825793e56a0f5b6fec2abe
Sean Donnelly <sdonne5@students.towson.edu>
2017-11-29 18:01:34 -0500

    Set Ciphertext in Runner

--- ProjectRunner.java
+++ ProjectRunner.java
@@ -1,6 +1,42 @@
+import java.util.HashMap;
+
 public class ProjectRunner {
+  HashMap<int,int> ciphertext = new HashMap<int,int>();
+  
   public static void main(String arg[]){
+    setCipertext();
 
+  }
 
+  public void setCipertext() {
+    ciphertext.put(3781, 14409);   ciphertext.put(31552, 3930);   ciphertext.put(27214, 15442);  ciphertext.put(5809, 30274);    
+    ciphertext.put(5400, 31486);   ciphertext.put(19936, 721);    ciphertext.put(27765, 29284);  ciphertext.put(29820, 7710);    
+    ciphertext.put(31590, 26470);  ciphertext.put(3781, 14409);   ciphertext.put(15898, 30844);  ciphertext.put(19048, 12914);   
+    ciphertext.put(16160, 3129);   ciphertext.put(301, 17252);    ciphertext.put(24689, 7776);   ciphertext.put(28856, 15720);   
+    ciphertext.put(30555, 24611);  ciphertext.put(20501, 2922);   ciphertext.put(13659, 5015);   ciphertext.put(5740, 31233);   
+    ciphertext.put(1616, 14170);   ciphertext.put(4294, 2307);    ciphertext.put(2320, 29174);   ciphertext.put(3036, 20132);   
+     ciphertext.put(14130, 22010);  ciphertext.put(25910, 19663);  ciphertext.put(19557, 10145);  ciphertext.put(18899, 27609);   
+    ciphertext.put(26004, 25056);  ciphertext.put(5400, 31486);   ciphertext.put(9526, 3019);    ciphertext.put(12962, 15189);   
+    ciphertext.put(29538, 5408);   ciphertext.put(3149, 7400);    ciphertext.put(9396, 3058);    ciphertext.put(27149, 20535);   
+    ciphertext.put(1777, 8737);    ciphertext.put(26117, 14251);  ciphertext.put(7129, 18195);   ciphertext.put(25302, 10248);  
+     ciphertext.put(23258, 3468);   ciphertext.put(26052, 20545);  ciphertext.put(21958, 5713);   ciphertext.put(346, 31194);     
+    ciphertext.put(8836, 25898);   ciphertext.put(8794, 17358);   ciphertext.put(1777, 8737);    ciphertext.put(25038, 12483);   
+    ciphertext.put(10422, 5552);   ciphertext.put(1777, 8737);    ciphertext.put(3780, 16360);   ciphertext.put(11685, 133);     
+    ciphertext.put(25115, 10840);  ciphertext.put(14130, 22010);  ciphertext.put(16081, 16414);  ciphertext.put(28580, 20845);   
+    ciphertext.put(23418, 22058);  ciphertext.put(24139, 9580);   ciphertext.put(173, 17075);    ciphertext.put(2016, 18131);    
+    ciphertext.put(19886, 22344);  ciphertext.put(21600, 25505);  ciphertext.put(27119, 19921);  ciphertext.put(23312, 16906);   
+    ciphertext.put(21563, 7891);   ciphertext.put(28250, 21321);  ciphertext.put(28327, 19237);  ciphertext.put(15313, 28649);   
+    ciphertext.put(24271, 8480);   ciphertext.put(26592, 25457);  ciphertext.put(9660, 7939);    ciphertext.put(10267, 20623);   
+    ciphertext.put(30499, 14423);  ciphertext.put(5839, 24179);   ciphertext.put(12846, 6598);   ciphertext.put(9284, 27858);    
+    ciphertext.put(24875, 17641);  ciphertext.put(1777, 8737);    ciphertext.put(18825, 19671);  ciphertext.put(31306, 11929);   
+    ciphertext.put(3576, 4630);    ciphertext.put(26664, 27572);  ciphertext.put(27011, 29164);  ciphertext.put(22763, 8992);   
+    ciphertext.put(3149, 7400);    ciphertext.put(8951, 29435);   ciphertext.put(2059, 3977);    ciphertext.put(16258, 30341);   
+    ciphertext.put(21541, 19004);  ciphertext.put(5865, 29526);   ciphertext.put(10536, 6941);   ciphertext.put(1777, 8737);    
+     ciphertext.put(17561, 11884);  ciphertext.put(2209, 6107);    ciphertext.put(10422, 5552);   ciphertext.put(19371, 21005);  
+     ciphertext.put(26521, 5803);   ciphertext.put(14884, 14280);  ciphertext.put(4328, 8635);    ciphertext.put(28250, 21321);   
+    ciphertext.put(28327, 19237);  ciphertext.put(15313, 28649);  
+    
+    
+    
   }
 }
